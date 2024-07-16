4f7ef6282a05b38a56372fd0fc5d46ada08ee2f7
Taehun Kim <bigteach0508@pusan.ac.kr>
2024-07-16 17:54:45 +0900

    Update model.h

--- 4-anomaly_detection_on_esp32_ver2/esp32_anomaly_detection/main/model.h
+++ 4-anomaly_detection_on_esp32_ver2/esp32_anomaly_detection/main/model.h
@@ -1,7 +1,7 @@
 #ifndef TENSORFLOW_LITE_MODEL_H
 #define TENSORFLOW_LITE_MODEL_H
 
-//#define MODEL_INT8
+#define MODEL_INT8
 
 extern const unsigned char model_tflite[];
 extern const int model_tflite_len;
